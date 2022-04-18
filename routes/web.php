b373bacb404f5e55943191b49990afda0e0d2b07
Andrey Portnov <asportnov@avito.ru>
2022-04-18 19:38:12 +0400

    tournament

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [\App\Http\Controllers\TournamentController::class, 'index']);
+Route::post('/tournament/create', [\App\Http\Controllers\TournamentController::class, 'create']);
+Route::get('/tournament/{uuid}', [\App\Http\Controllers\TournamentController::class, 'get']);
+Route::get('/tournament/result/{uuid}', [\App\Http\Controllers\TournamentController::class, 'result']);
+
+Route::post('/tournament/fixtures/{uuid}', [\App\Http\Controllers\TournamentController::class, 'createFixtures']);
+Route::get('/tournament/fixtures/{uuid}', [\App\Http\Controllers\TournamentController::class, 'getFixtures']);
+
+Route::post('/tournament/playAll/{uuid}', [\App\Http\Controllers\TournamentController::class, 'playAll']);
+
+Route::get('/tournament/result/{$uuid}', [\App\Http\Controllers\TournamentController::class, 'resultAll']);
+
+Route::get('/tournament/teams', [\App\Http\Controllers\TournamentController::class, 'getTeams']);
+
+
